a6e9f12ec40633a93e6f1abba3526f262de26052
ElLuchoMan <luchoman96@gmail.com>
2021-02-26 12:06:07 -0500

    Se agrega el primer estado, definiendo el tamaño del panel

--- TapeteDeSierpinski.m
+++ TapeteDeSierpinski.m
@@ -3,3 +3,6 @@ clear all
 clc
 n = 7;%Cantidad de estados a mostrar
 f = cell(1, n); %Arreglo de celdas para guardar las matrices
+f{1} = ones(300 * n );%tamaño del panel
+figure,imshow(f{1}); %Imprime en pantalla cada uno de los estados generados
+%Permite realizar cada estado
