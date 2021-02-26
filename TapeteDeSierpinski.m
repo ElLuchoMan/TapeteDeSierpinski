b824076f5938ffb1908c4204cb590c8addc80bf1
ElLuchoMan <luchoman96@gmail.com>
2021-02-26 12:07:04 -0500

    Se agrega el for para el número de estados, llamando a la función T que implementaremos luego

--- TapeteDeSierpinski.m
+++ TapeteDeSierpinski.m
@@ -6,3 +6,7 @@ f = cell(1, n); %Arreglo de celdas para guardar las matrices
 f{1} = ones(300 * n );%tamaño del panel
 figure,imshow(f{1}); %Imprime en pantalla cada uno de los estados generados
 %Permite realizar cada estado
+for i = 1: n - 1
+    f{i+1} = T(f{i});
+    figure,imshow(f{i + 1});
+end
